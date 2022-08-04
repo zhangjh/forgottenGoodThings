5f6df94d8a3a7a84769b7cc185db900673092161
root <root@localhost.localdomain>
2022-08-04 15:11:48 +0800

    update

--- nas/start.sh
+++ nas/start.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-mkdir -vp /app/nextcloud/{mysql,data} /app/onlyoffice/{logs,data}
-cp -r onlyoffice /app/nextcloud/data/apps
+mkdir -vp /app/nextcloud/mysql
+mkdir -vp /app/nextcloud/data 
+mkdir -vp /app/onlyoffice/logs
+mkdir -vp /app/onlyoffice/data
 
 docker-compose up -d
+
+while [ true ];do
+	docker ps | grep nextcloud
+	if [ $? -eq 0 ];then
+		cd plugins
+		ls * | xargs -n1 tar -xf
+		cp -r `ls -d */` /app/nextcloud/data/apps
+
+		dirs=`ls -d */`
+		if [ "$dirs" != "" ];then
+			rm -rf ${dirs}
+		fi	
+		break;
+	fi
+done
+
+
