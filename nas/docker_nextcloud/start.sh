4042e946a64792fcb2c5e55c6a1655d455f76cd6
njhxzhangjh <njhxzhangjihong@126.com>
2023-02-01 20:43:40 +0800

    add frpc

--- nas/docker_nextcloud/start.sh
+++ nas/docker_nextcloud/start.sh
@@ -7,20 +7,20 @@ mkdir -vp /app/onlyoffice/data
 #chown -R www-data:www-data /home/zhangjh
 docker-compose up -d
 
-while [ true ];do
-	docker ps | grep nextcloud
-	if [ $? -eq 0 ];then
-		cd plugins
-		ls * | xargs -n1 tar -xf
-		cp -r `ls -d */` /app/nextcloud/data/apps
+#while [ true ];do
+	#docker ps | grep nextcloud
+	#if [ $? -eq 0 ];then
+	#	cd plugins
+	#	ls * | xargs -n1 tar -xf
+	#	cp -r `ls -d */` /app/nextcloud/data/apps
 
-		dirs=`ls -d */`
-		if [ "$dirs" != "" ];then
-			rm -rf ${dirs}
-		fi	
-		break;
-	fi
-done
+	#	dirs=`ls -d */`
+	#	if [ "$dirs" != "" ];then
+	#		rm -rf ${dirs}
+	#	fi	
+	#	break;
+	#fi
+#done
 
 ## 重启时同步挂载盘
-docker exec --user www-data nas_nextcloud_1 php occ files:scan --all
+docker exec --user www-data nextcloud php occ files:scan --all
