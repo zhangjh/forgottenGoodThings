99854ad86d5d60b9ef5187fda2c6e43cf03e5a31
zhangjh <njhxzhangjihong@126.com>
2024-03-18 15:06:33 +0800

    update

--- nas/docker_nextcloud/start.sh
+++ nas/docker_nextcloud/start.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-mkdir -vp /app/nextcloud/mysql
-mkdir -vp /app/nextcloud/data 
-mkdir -vp /app/onlyoffice/logs
-mkdir -vp /app/onlyoffice/data
+#mkdir -vp /app/nextcloud/mysql
+#mkdir -vp /app/nextcloud/data 
+#mkdir -vp /app/onlyoffice/logs
+#mkdir -vp /app/onlyoffice/data
 
 #chown -R www-data:www-data /home/zhangjh
 docker-compose up -d
 
 #while [ true ];do
-	#docker ps | grep nextcloud
-	#if [ $? -eq 0 ];then
-	#	cd plugins
-	#	ls * | xargs -n1 tar -xf
-	#	cp -r `ls -d */` /app/nextcloud/data/apps
-
-	#	dirs=`ls -d */`
-	#	if [ "$dirs" != "" ];then
-	#		rm -rf ${dirs}
-	#	fi	
-	#	break;
-	#fi
+#	docker ps | grep nextcloud
+#	if [ $? -eq 0 ];then
+#		cd plugins
+#		ls * | xargs -n1 tar -xf
+#		cp -r `ls -d */` /app/nextcloud/data/apps
+#
+#		dirs=`ls -d */`
+#		if [ "$dirs" != "" ];then
+#			rm -rf ${dirs}
+#		fi	
+#		break;
+#	fi
 #done
 
 ## 重启时同步挂载盘
